a3dee4af1ce691b4d342d078dc258311a63efeb9
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-04 22:31:27 -0300

    feat: ensure SignUpController returns an error if no name is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,15 @@
+import { MissingParamError } from '../errors';
+import { badRequest } from '../helpers/http-helper';
+import { HttpResponse } from '../protocols/http';
+
 export class SignUpController {
-  handle() {
-    return { statusCode: 400 };
+  handle(httpRequest: any): HttpResponse | null {
+    const requiredFields = ['name'];
+    for (const field of requiredFields) {
+      if (!httpRequest?.body?.[field]) {
+        return badRequest(new MissingParamError(field));
+      }
+    }
+    return null;
   }
 }
