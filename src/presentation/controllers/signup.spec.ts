4b9ed225fda051d0f4a76c2e3555cb07778c809a
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-04 22:37:20 -0300

    test: ensure SignUpController returns 400 if no e-mail is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -6,13 +6,31 @@ describe('SignUp Controller', () => {
     const sut = new SignUpController();
 
     const httpRequest = {
-      email: 'any_email',
-      password: 'any_password',
+      body: {
+        email: 'any_email',
+        password: 'any_password',
+      },
     };
 
     const httpResponse = sut.handle(httpRequest);
 
     expect(httpResponse?.statusCode).toBe(400);
     expect(httpResponse?.body).toEqual(new MissingParamError('name'));
+  });
+
+  it('should return 400 if no email is provided', () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse?.statusCode).toBe(400);
+    expect(httpResponse?.body).toEqual(new MissingParamError('email'));
   });
 });
