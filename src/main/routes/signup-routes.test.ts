d46de2477c716c52567deb351590c9719e479914
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-20 09:42:23 -0300

    refactor: connect to database before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,8 +1,21 @@
 import { faker } from '@faker-js/faker';
 import request from 'supertest';
+import { MySQLHelper } from '../../infra/database/my-sql/helpers/my-sql-helper';
 import { app } from '../config/app';
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MySQLHelper.connect();
+  });
+
+  afterAll(async () => {
+    await MySQLHelper.disconnect();
+  });
+
+  afterEach(async () => {
+    await MySQLHelper.connection('users').delete();
+  });
+
   it('should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
