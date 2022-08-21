a257b8292c1ce110307347446f487b1c65c91b8d
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-21 19:51:17 -0300

    feat: ensure LoginController returns server error if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
-import { InvalidParamError, MissingParamError } from '../../errors';
-import { badRequest, ok } from '../../helpers/http-helper';
+import { InvalidParamError, MissingParamError, ServerError } from '../../errors';
+import { badRequest, ok, serverError } from '../../helpers/http-helper';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { EmailValidator } from '../signup/signup-protocols';
 
@@ -18,11 +18,15 @@ export class LoginController implements Controller {
       }
     }
 
-    const isValid = this.emailValidator.isValid(httpRequest?.body?.email);
-    if (!isValid) {
-      return new Promise((resolve) => resolve(badRequest(new InvalidParamError('email'))));
-    }
+    try {
+      const isValid = this.emailValidator.isValid(httpRequest?.body?.email);
+      if (!isValid) {
+        return new Promise((resolve) => resolve(badRequest(new InvalidParamError('email'))));
+      }
 
-    return new Promise((resolve) => resolve(ok({})));
+      return new Promise((resolve) => resolve(ok({})));
+    } catch (error) {
+      return new Promise((resolve) => resolve(serverError(error as Error)));
+    }
   }
 }
