f7cbcf7f1980412895181923727659f7b8a16aa6
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-21 17:30:53 -0300

    refactor: remove duplicated code

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,15 +1,51 @@
+/* eslint-disable max-classes-per-file */
+import { LogErrorRepository } from '../../data/protocols/database/log-error-repository';
+import { AccountModel } from '../../domain/models/account';
+import { ok, serverError } from '../../presentation/helpers/http-helper';
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols';
 import { LogControllerDecorator } from './log';
+
+const makeFakeHttpRequest = (): HttpRequest => ({
+  body: {
+    name: 'any_name',
+    email: 'any_email',
+    password: 'any_password',
+  },
+});
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email',
+  password: 'any_password',
+});
+
+const makeFakeServerError = (): HttpResponse => {
+  const fakeError = new Error();
+  fakeError.stack = 'any_stack';
+  return serverError(fakeError);
+};
 
 interface SutTypes {
   controllerStub: Controller;
   sut: LogControllerDecorator;
+  logErrorRepositoryStub: LogErrorRepository;
 }
+
+const makeLogErrorRepository = (): LogErrorRepository => {
+  class LogErrorRepositoryStub implements LogErrorRepository {
+    async log(stack: string): Promise<void> {
+      return new Promise((resolve) => resolve());
+    }
+  }
+
+  return new LogErrorRepositoryStub();
+};
 
 const makeController = (): Controller => {
   class ControllerStub implements Controller {
     handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-      return new Promise((resolve) => resolve({ statusCode: 200, body: {} }));
+      return new Promise((resolve) => resolve(ok(makeFakeAccount())));
     }
   }
 
@@ -17,9 +53,10 @@ const makeController = (): Controller => {
 };
 
 const makeSut = (): SutTypes => {
+  const logErrorRepositoryStub = makeLogErrorRepository();
   const controllerStub = makeController();
-  const sut = new LogControllerDecorator(controllerStub);
-  return { controllerStub, sut };
+  const sut = new LogControllerDecorator(controllerStub, logErrorRepositoryStub);
+  return { controllerStub, sut, logErrorRepositoryStub };
 };
 
 describe('LogControllerDecorator', () => {
@@ -27,17 +64,28 @@ describe('LogControllerDecorator', () => {
     const { controllerStub, sut } = makeSut();
     const handleSpy = jest.spyOn(controllerStub, 'handle');
 
-    const httpRequest = { body: { name: 'any_name' } };
+    const httpRequest = makeFakeHttpRequest();
     await sut.handle(httpRequest);
     expect(handleSpy).toHaveBeenCalledWith(httpRequest);
   });
 
   it('should return the same result of the controller', async () => {
     const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeFakeHttpRequest());
 
-    const httpRequest = { body: { name: 'any_name' } };
-    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(ok(makeFakeAccount()));
+  });
 
-    expect(httpResponse).toEqual({ statusCode: 200, body: {} });
+  it('should call LogErrorRepository if controller returns a server error', async () => {
+    const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
+
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'log');
+    jest
+      .spyOn(controllerStub, 'handle')
+      .mockReturnValueOnce(new Promise((resolve) => resolve(makeFakeServerError())));
+
+    await sut.handle(makeFakeHttpRequest());
+
+    expect(logSpy).toHaveBeenCalledWith('any_stack');
   });
 });
