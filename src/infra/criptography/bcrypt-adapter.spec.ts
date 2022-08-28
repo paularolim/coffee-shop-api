0f320405f85e12ebc89c5d6052653d91399bc9c3
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-28 13:23:20 -0300

    test: ensure BcryptAdapter returns false when compare fails

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -53,4 +53,14 @@ describe('Bcrypt Adapter', () => {
 
     expect(isValid).toBe(true);
   });
+
+  it('should return false when compare fails', async () => {
+    const sut = makeSut();
+    jest
+      .spyOn(bcrypt, 'compare')
+      .mockReturnValueOnce(new Promise((resolve) => resolve(false)) as any);
+    const isValid = await sut.compare('any_value', 'any_hash');
+
+    expect(isValid).toBe(false);
+  });
 });
