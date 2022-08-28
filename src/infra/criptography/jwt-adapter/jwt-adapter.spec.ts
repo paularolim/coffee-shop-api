cffc42350a93f08faaef32e2daf123d7d0a49b08
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-28 14:15:16 -0300

    test: ensure JwtAdapter calls sign with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,11 +1,21 @@
 import jwt from 'jsonwebtoken';
 import { JwtAdapter } from './jwt-adapter';
 
+jest.mock('jsonwebtoken', () => ({
+  sign: (): Promise<string> => new Promise((resolve) => resolve('any_token')),
+}));
+
 describe('Jwt Adapter', () => {
   it('should call sign with correct values', async () => {
     const sut = new JwtAdapter('secret');
     const signSpy = jest.spyOn(jwt, 'sign');
     await sut.encrypt('any_id');
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
+  });
+
+  it('should return a token on sign success', async () => {
+    const sut = new JwtAdapter('secret');
+    const token = await sut.encrypt('any_id');
+    expect(token).toBe('any_token');
   });
 });
