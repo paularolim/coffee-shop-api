bdbc181cf030160da0dfe1a3ac27ed3fa7aaff8f
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-28 14:44:14 -0300

    test: ensure AccountMySQLRepository returns an account on loadByEmail succeeds

--- src/infra/database/my-sql/account-repository/account.spec.ts
+++ src/infra/database/my-sql/account-repository/account.spec.ts
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker';
+import { v4 as uuid } from 'uuid';
 import { MySQLHelper } from '../helpers/my-sql-helper';
 import { AccountMySQLRepository } from './account';
 
@@ -15,7 +16,7 @@ describe('Account MySQL repository', () => {
     await MySQLHelper.connection('users').delete();
   });
 
-  it('should return an account on success', async () => {
+  it('should return an account on add success', async () => {
     const sut = new AccountMySQLRepository();
 
     const accountMock = {
@@ -30,5 +31,22 @@ describe('Account MySQL repository', () => {
     expect(account?.name).toBe(accountMock.name);
     expect(account?.email).toBe(accountMock.email);
     expect(account?.password).toBe(accountMock.password);
+  });
+
+  it('should return an account on load success', async () => {
+    const sut = new AccountMySQLRepository();
+    const id = uuid();
+
+    await MySQLHelper.connection
+      .insert({ id, name: 'any_name', email: 'any_email', password: 'any_password' })
+      .into('users');
+
+    const account = await sut.loadByEmail('any_email');
+
+    expect(account).toBeTruthy();
+    expect(account?.id).toBe(id);
+    expect(account?.name).toBe('any_name');
+    expect(account?.email).toBe('any_email');
+    expect(account?.password).toBe('any_password');
   });
 });
