967cc3578424c7d1af6e8ec21894c9a3cc403b22
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-28 14:20:27 -0300

    test: ensure JwtAdapter throw if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -18,4 +18,13 @@ describe('Jwt Adapter', () => {
     const token = await sut.encrypt('any_id');
     expect(token).toBe('any_token');
   });
+
+  it('should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret');
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const promise = sut.encrypt('any_id');
+    await expect(promise).rejects.toThrow();
+  });
 });
