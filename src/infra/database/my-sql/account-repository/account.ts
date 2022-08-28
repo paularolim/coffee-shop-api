ec49b2af3703dfde6eb73f3753d3a076f782423c
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-28 14:43:19 -0300

    feat: ensure AccountMySQLRepository returns an account on loadByEmail succeeds

--- src/infra/database/my-sql/account-repository/account.ts
+++ src/infra/database/my-sql/account-repository/account.ts
@@ -1,10 +1,11 @@
 import { v4 as uuid } from 'uuid';
 import { AddAccountRepository } from '../../../../data/protocols/database/account/add-account-repository';
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/database/authentication/load-account-by-email-repository';
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { MySQLHelper } from '../helpers/my-sql-helper';
 
-export class AccountMySQLRepository implements AddAccountRepository {
+export class AccountMySQLRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add(account: AddAccountModel): Promise<AccountModel | null> {
     const id = uuid();
 
@@ -16,5 +17,15 @@ export class AccountMySQLRepository implements AddAccountRepository {
       .first()
       .then((result) => result);
     return inserted || null;
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel | null> {
+    const searched = await MySQLHelper.connection
+      .select()
+      .from<AccountModel>('users')
+      .where('email', email)
+      .first()
+      .then((result) => result);
+    return searched || null;
   }
 }
