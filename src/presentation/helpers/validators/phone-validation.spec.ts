7826114aa2d5430e2910b409f4c390c23bfc4383
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-30 22:24:44 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/helpers/validators/phone-validation.spec.ts
+++ src/presentation/helpers/validators/phone-validation.spec.ts
@@ -2,17 +2,31 @@ import { InvalidParamError } from '../../errors';
 import { PhoneValidator } from '../../protocols/phone-validator';
 import { PhoneValidation } from './phone-validation';
 
+interface SutTypes {
+  phoneValidatorStub: PhoneValidator;
+  sut: PhoneValidation;
+}
+
+const makePhoneValidator = (): PhoneValidator => {
+  class PhoneValidatorStub implements PhoneValidator {
+    isValid(phone: string): boolean {
+      return false;
+    }
+  }
+
+  return new PhoneValidatorStub();
+};
+
+const makeSut = (): SutTypes => {
+  const phoneValidatorStub = makePhoneValidator();
+  const sut = new PhoneValidation('phone', phoneValidatorStub);
+  return { sut, phoneValidatorStub };
+};
+
 describe('PhoneValidation', () => {
   it('should return an error if PhoneValidator returns false', () => {
-    class PhoneValidationStub implements PhoneValidator {
-      isValid(phone: string): boolean {
-        return false;
-      }
-    }
-
-    const phoneValidationStub = new PhoneValidationStub();
-    const sut = new PhoneValidation('phone', phoneValidationStub);
-    jest.spyOn(phoneValidationStub, 'isValid').mockReturnValueOnce(false);
+    const { sut, phoneValidatorStub } = makeSut();
+    jest.spyOn(phoneValidatorStub, 'isValid').mockReturnValueOnce(false);
 
     const error = sut.validate({ phone: 'any_phone' });
 
