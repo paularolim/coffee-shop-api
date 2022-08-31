813bbac190835d9fece97746d336802669597f53
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-30 22:01:00 -0300

    test: ensure PhoneValidatorAdapter returns true if validator return true

--- src/main/adapters/validators/phone-validator-adapter.spec.ts
+++ src/main/adapters/validators/phone-validator-adapter.spec.ts
@@ -1,9 +1,23 @@
+import validator from 'validator';
 import { PhoneValidatorAdapter } from './phone-validator-adapter';
+
+jest.mock('validator', () => ({
+  isMobilePhone(): boolean {
+    return true;
+  },
+}));
 
 describe('PhoneValidatorAdapter', () => {
   it('should return false if validator returns false', () => {
-    const sut = new PhoneValidatorAdapter();
+    const sut = new PhoneValidatorAdapter('pt-BR');
+    jest.spyOn(validator, 'isMobilePhone').mockReturnValueOnce(false);
     const isValid = sut.isValid('any_phone');
     expect(isValid).toBe(false);
+  });
+
+  it('should return true if validator returns true', () => {
+    const sut = new PhoneValidatorAdapter('pt-BR');
+    const isValid = sut.isValid('any_phone');
+    expect(isValid).toBe(true);
   });
 });
