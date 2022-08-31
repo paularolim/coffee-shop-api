5850253b74fa3739f6a081167d27313afe46d6f0
Paula Rolim <rolim.paula1999@gmail.com>
2022-08-30 22:00:29 -0300

    feat: ensure PhoneValidatorAdapter returns true if validator return true

--- src/main/adapters/validators/phone-validator-adapter.ts
+++ src/main/adapters/validators/phone-validator-adapter.ts
@@ -1,7 +1,10 @@
+import validator from 'validator';
 import { PhoneValidator } from '../../../presentation/protocols/phone-validator';
 
 export class PhoneValidatorAdapter implements PhoneValidator {
+  constructor(private readonly locale: validator.PhoneLocale) {}
+
   isValid(phone: string): boolean {
-    return false;
+    return validator.isMobilePhone(phone, this.locale);
   }
 }
